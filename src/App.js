ebef9339fa7ca830149cc4a1213b2d463cb1e272
Raluca Ulmeanu <raluca.ioana.ulmeanu@gmail.com>
2023-06-17 23:46:45 +0300

    first commit & git commands doc updated

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'; //React component is responsible for interpreting jsx code. 
+//The React import isn't necessary to be added as it will be added automatically by the newer React versions
+import './App.css';
+
+const App = () => {
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>project git repository</p>
+      </header>
+      <h2>Project Name</h2>
+    </div>
+  );
+}
+
+export default App;
